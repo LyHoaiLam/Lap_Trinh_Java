4d82db5bd3caadb86fcac37924937b913f3c5156
Your Name <hoailam03999@gmail.com>
2022-01-13 14:50:01 +0700

    UP

--- src/Student/StudentK14_TestDrive.java
+++ src/Student/StudentK14_TestDrive.java
@@ -6,14 +6,23 @@ public class StudentK14_TestDrive
 {
     public static void main(String[] args) 
     {
+        Scanner input = new Scanner(System.in);
         StudenK14 studenK14 = new StudenK14();
         
-        Scanner input = new Scanner(System.in);
-        int chucNang1;
+        int cs = 5;
+        while(cs != 2 )
+        {
+            studenK14.nhapThongTinStudent();      
+            studenK14.show();
+            System.out.println("1. Nhap Tieo ");
+            System.out.println("2. Thoat Ra");
+            cs = input.nextInt();
+            System.out.println("-----------------------------------------");
+        }
         
-        studenK14.nhapThongTinStudent();
+        //studenK14.nhapThongTinStudent();
        
-        studenK14.show();
-
+        //studenK14.show();
+       
     }    
 }
