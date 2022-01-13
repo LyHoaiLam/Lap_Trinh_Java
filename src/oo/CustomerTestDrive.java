398a365455dd5c8af133d38de068118a167e4d8d
Your Name <hoailam03999@gmail.com>
2022-01-13 14:05:47 +0700

    up

---
+++ src/oo/CustomerTestDrive.java
@@ -0,0 +1,11 @@
+package oo;
+
+public class CustomerTestDrive 
+{
+    public static void main(String[] args) 
+    {
+        Customer customer = new Customer();
+        customer.nhaptt();
+        customer.show();    
+    }    
+}
--- src/oo/Employee.java
+++ src/oo/Employee.java
@@ -16,6 +16,7 @@ public class Employee
         name = input.nextLine();
         System.out.println("Nhap Tuoi: ");
         old = input.nextInt();
+        input.nextLine();
         System.out.println("Nhap Dia Chi: ");
         address = input.nextLine();
     }
